7febc317a2eb5898e46bf8b18e33b2ea33c2108f
John T. Wodder II <git@varonathe.org>
2020-03-16 14:26:00 +0000

    Run tests

--- test/test_jwodder_style.py
+++ test/test_jwodder_style.py
@@ -1,9 +1,12 @@
 import ast
-from   pathlib                     import Path
+import os
+from   os.path                     import dirname, join
 import re
-import pytest
 from   flake8_import_order.checker import ImportOrderChecker
 from   flake8_import_order.styles  import lookup_entry_point
+import pytest
+
+DATA_DIR = join(dirname(__file__), 'data')
 
 ERROR_RGX = re.compile("# ((?:I[0-9]{3} ?)+) ?.*$")
 
@@ -17,19 +20,18 @@ options = {
     'import_order_style': style_entry_point,
 }
 
-@pytest.mark.parametrize('pyfile', [
-    p for p in Path(__file__).with_name('data').iterdir()
-      if p.suffix == '.whl'
-], ids=attrgetter("name"))
+@pytest.mark.parametrize('pyfile', os.listdir(DATA_DIR))
 def test_jwodder_style(pyfile):
-    data = pyfile.read_text()
+    pypath = join(DATA_DIR, pyfile)
+    with open(pypath) as fp:
+        data = fp.read()
     expected = []
     for line in data.splitlines():
         m = ERROR_RGX.search(line)
         if m:
             expected.extend(m.group(1).split())
-    tree = ast.parse(data, fullpath)
-    checker = ImportOrderChecker(str(pyfile), tree)
+    tree = ast.parse(data, pypath)
+    checker = ImportOrderChecker(pypath, tree)
     checker.options = options
     codes = [error.code for error in checker.check_order()]
     assert codes == expected
