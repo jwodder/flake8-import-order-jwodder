a3213b4506faa7e87b5b5fad563a966754862caf
John T. Wodder II <git@varonathe.org>
2020-03-16 03:49:11 +0000

    Packaging

--- flake8_import_order_jwodder.py
+++ flake8_import_order_jwodder.py
@@ -1,3 +1,16 @@
+"""
+Yet another import order style
+
+Visit <https://github.com/jwodder/flake8-import-order-jwodder> for more
+information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'flake8-import-order-jwodder@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/flake8-import-order-jwodder'
+
 from flake8_import_order.styles import AppNexus, Google
 
 class JWodder(AppNexus):
